f77bcde7e0aed3a7ccfb426b4ee723aa110ebe83
kinotikm <cloutkevin254@gmail.com>
2024-06-21 13:52:18 +0300

    finall commits

--- database/connection.py
+++ database/connection.py
@@ -6,3 +6,8 @@ def get_db_connection():
     conn = sqlite3.connect(DATABASE_NAME)
     conn.row_factory = sqlite3.Row
     return conn
+def get_db_connection():
+
+    conn = sqlite3.connect('magazine.db')
+
+    return conn
